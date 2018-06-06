c4ca990cb4fb0ece2d69c259907c0a807f45429b
jfmiguez <miguez@gmail.com>
2018-06-06 07:44:36 -0400

    Data for Bases established as class
    This class will have some data pertaining to mosts base patterns.

---
+++ AutoMarket/AutoMarket/Bases/BaseData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMarket.Bases
+{
+    public class BaseData
+    {
+        public String BaseName { set; get; }
+        public DateTime DateBaseStarts { set; get; }
+        public DateTime DateBaseEnds { set; get; }
+        public Decimal HeightBaseStarts { set; get; }
+        public Decimal HeightBaseEnds { set; get; }
+        public Decimal HeightMidpointBase { set; get; }
+        public Decimal DepthBasePercent { set; get; }
+        public Decimal PivotPoint { set; get; }
+        public DateTime DateHandleStarts { set; get; }
+        public Decimal DepthHandlePercent { set; get; }
+    }
+}
